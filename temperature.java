83255a9c84c3a0fd5661f2d19b5d963a989a2c07
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-07-06 21:55:29 -0400

    started with java version

---
+++ temperature.java
@@ -0,0 +1,12 @@
+/*
+* Temperature converter from Fahrenheit to Celcius
+* Author: Gonzalo Vazquez
+*/
+
+public class Temperature() {
+	public int temp;
+
+	public int Calculate(temperature) {
+		
+	}
+}
