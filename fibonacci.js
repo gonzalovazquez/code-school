376dc56d1a2dac869953db71367a421d53e317d9
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-07-14 21:45:29 -0400

    added fibonacci project

---
+++ fibonacci.js
@@ -0,0 +1,9 @@
+// is formed by adding the latest two numbers to get the next one, starting from 0 and 1:
+ 
+//   0 1 --the series starts like this.
+//   0+1=1 so the series is now 
+//   0 1 1
+//     1+1=2 so the series continues...
+//   0 1 1 2 and the next term is
+//       1+2=3 so we now have
+//   0 1 1 2 3  and it continues as follows ...
