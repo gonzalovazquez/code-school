ed2761ca82eb1d78ffd3353b1735d4ceaa067f60
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-07-24 23:20:57 -0400

    added ruby version of fibonacci

---
+++ test-fibonacci.rb
@@ -0,0 +1,9 @@
+class TestFibonacci
+
+	require_relative 'fibonacci'
+
+	f = Fibonacci.new()
+
+	f.create(100)
+
+end
--- testTemperature.java
+++
@@ -1,8 +0,0 @@
-public class TestTemperature {
-	public static void main (String [] args) {
-		//Instansiate new Temperature
-		Temperature myTemp = new Temperature(78);
-		int myTemperatureIs = myTemp.toFarhenheit();
-		System.out.println(" My Temperature is " + myTemperatureIs);
-	}
-}
