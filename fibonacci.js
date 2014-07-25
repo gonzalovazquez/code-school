35649aa1b465d6737a7f726f729388d9d4c9ed3b
Gonzalo Vazquez <gonzalo.segura1@gmail.com>
2014-07-24 22:14:38 -0400

    fibonacci sequence in javascript

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,13 @@
-// is formed by adding the latest two numbers to get the next one, starting from 0 and 1:
- 
-//   0 1 --the series starts like this.
-//   0+1=1 so the series is now 
-//   0 1 1
-//     1+1=2 so the series continues...
-//   0 1 1 2 and the next term is
-//       1+2=3 so we now have
-//   0 1 1 2 3  and it continues as follows ...
+function createFibonacci(limit) {
+	var a = 0, b = 1, seq = [0 , 1], num = 0, nextNum;
+
+	while(num < limit) {
+		nextNum = a + b;
+		seq.push(nextNum);
+		a = b;
+		b = nextNum;
+		num++;
+	}
+
+	console.log(seq);
+}
